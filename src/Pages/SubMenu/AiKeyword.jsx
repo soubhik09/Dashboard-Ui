71df92bdb3ba590b67218ecb6ad31b85f9766a09
Soubhik <soubhikpramanik10@gmail.com>
2025-05-19 18:25:05 +0530

    Fixed Submenu's Loading Skeleton issue

--- src/Pages/SubMenu/AiKeyword.jsx
+++ src/Pages/SubMenu/AiKeyword.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function AiKeyword() {
   return (
     <div className="h-full flex justify-center items-center text-5xl">
-      <div className="mt-150 md:mt-0 bg-red-400">
+      <div className="mt-150 md:mt-0">
         <ArticleSkeleton />
       </div>
     </div>
--- src/Pages/SubMenu/ImportKeyword.jsx
+++ src/Pages/SubMenu/ImportKeyword.jsx
@@ -1,10 +1,14 @@
-import ArticleSkeleton from '@/components/ArticleSkeleton'
-import React from 'react'
+import ArticleSkeleton from "@/components/ArticleSkeleton";
+import React from "react";
 
 function ImportKeyword() {
   return (
-    <div className='h-full flex justify-center items-center text-5xl'><ArticleSkeleton /></div>
-  )
+    <div className="h-full flex justify-center items-center text-5xl">
+      <div className="mt-150 md:mt-0">
+        <ArticleSkeleton />
+      </div>
+    </div>
+  );
 }
 
-export default ImportKeyword
+export default ImportKeyword;
